a2e0eabf94692905313f3984fd8dd82452a84853
gift56 <efeasiughu@gmail.com>
2023-01-05 14:07:32 -0800

    done writing unique id generator function

--- client/script.js
+++ client/script.js
@@ -44,6 +44,6 @@ const generateUniqueId = () => {
 // ChatStripe between user and robot
 const chatStripe = (isAi, value, uniqueId) => {
   return `
-     <div></div>
+     <div class=""></div>
   `;
 };
