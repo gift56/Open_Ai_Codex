63ac6e53bb277462a9de687300e34316bfc4812d
gift56 <efeasiughu@gmail.com>
2023-01-05 13:45:04 -0800

    importing some stuff

--- client/script.js
+++ client/script.js
@@ -2,3 +2,4 @@ import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
 
 const form = document.querySelector("form");
+const chatContainer=document.querySelector("#chat_container")
