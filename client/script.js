7b0440949350ff50e509023154b0990d160571d5
gift56 <efeasiughu@gmail.com>
2023-01-05 17:58:45 -0800

    intergrating frontend

--- client/script.js
+++ client/script.js
@@ -77,6 +77,9 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
+  // Fetching data from the server
+  
 };
 
 form.addEventListener("submit", handleSubmit);
