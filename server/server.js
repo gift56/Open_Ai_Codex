b0b52fb0d5bacbf14f106c167ce6fc759faf1b72
gift56 <efeasiughu@gmail.com>
2023-01-05 17:46:00 -0800

    working on express

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.post("/", async (req, res) => {
       temperature: 0,
       max_tokens: 3000,
       top_p: 1,
-      frequency_penalty: 0,
+      frequency_penalty: 0.5,
       presence_penalty: 0,
       stop: ['"""'],
     });
