f126f7115e0b04f6fa8220c73f4bb3631ab82f5f
gift56 <efeasiughu@gmail.com>
2023-01-05 17:37:13 -0800

    working on express

--- server/server.js
+++ server/server.js
@@ -15,4 +15,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req, res) => {});
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello World",
+  });
+});
