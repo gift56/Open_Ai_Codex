ef1dc5994b1c63c23b29ce517c103e1f6956c471
gift56 <efeasiughu@gmail.com>
2023-01-05 17:10:32 -0800

    done working on handle submit function

--- client/script.js
+++ client/script.js
@@ -80,3 +80,8 @@ const handleSubmit = async (e) => {
 };
 
 form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
