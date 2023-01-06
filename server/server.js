fe0820288e59f4a88b6b4429d543e20a9825b2c1
gift56 <efeasiughu@gmail.com>
2023-01-05 17:51:38 -0800

    done working on backend

--- server/server.js
+++ server/server.js
@@ -45,4 +45,6 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.listen(5000, () =>
+  console.log("Server is running on port http://localhost:5000")
+);
